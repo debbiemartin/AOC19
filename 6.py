d7f4dea10ce033b64b04e793d7eac6c3777621e1
Deborah Martin <debomart@cisco.com>
2020-10-01 02:15:06 -0700

    Made 6 use dicts for look up

--- 6.py
+++ 6.py
@@ -1,7 +1,7 @@
 #!/auto/ensoft/bin/python
 
 
-orbits = []
+orbits = {}
 planets = {}
 lookups = []
 
@@ -9,18 +9,20 @@ lookups = []
 def add_orbits(planet):
 
     orbit_num = planets[planet]
-    for pair in orbits:
-        if pair[0] == planet:
-            print(pair[1])
-            planets[pair[1]] = orbit_num + 1
-            add_orbits(pair[1])
+
+    if planet in orbits:
+        for orbiters in orbits[planet]:
+            planets[orbiters] = orbit_num + 1
+            add_orbits(orbiters)
 
 
 with open('orbits.txt', 'r') as f:
     for line in f:
         orbit_pair = line.split(")")
         orbit_pair[1] = orbit_pair[1].strip('\n')
-        orbits.append(orbit_pair)
+        if orbit_pair[0] not in orbits:
+            orbits[orbit_pair[0]] = []
+        orbits[orbit_pair[0]].append(orbit_pair[1])
 
         if orbit_pair[0] not in planets.keys():
             planets[orbit_pair[0]] = 0
@@ -30,7 +32,6 @@ with open('orbits.txt', 'r') as f:
         if orbit_pair[0] == "COM":
             lookups.append(orbit_pair[1])
             planets[orbit_pair[1]] = 1
-
 
 for lookup in lookups:
     add_orbits(lookup)
