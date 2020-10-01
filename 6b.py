12e07c995b8ee351fdfdfca0abdb196f719176ed
Deborah Martin <debomart@cisco.com>
2020-10-01 08:12:59 -0700

    finished 6b

--- 6b.py
+++ 6b.py
@@ -1,46 +1,45 @@
 #!/auto/ensoft/bin/python
 
+orbits = {} # planet with a list of orbiteds
+hops_from_you = {} # planet with shortest number of hops from you
+hops_from_san = {} # planet with shortest number of hops from santa
 
-orbits = {}
-planets = {}
-lookups = []
+def calculate_hops(planet, hops, lookup):
+    if not planet in lookup:
+        # not in the dictionary already - add it with num hops
+        lookup[planet] = hops
+    else:
+        # already got a number of hops, but we want the smallest
+        if hops < lookup[planet]:
+            lookup[planet] = hops
 
-
-def add_orbits(planet):
-
-    orbit_num = planets[planet]
-
-    if planet in orbits:
-        for orbiters in orbits[planet]:
-            planets[orbiters] = orbit_num + 1
-            add_orbits(orbiters)
-
+    if planet in orbits: # protects for "COM"
+        for orbitted_planet in orbits[planet]:
+            calculate_hops(orbitted_planet, hops + 1, lookup)
 
 with open('orbits.txt', 'r') as f:
     for line in f:
         orbit_pair = line.split(")")
         orbit_pair[1] = orbit_pair[1].strip('\n')
-        if orbit_pair[0] not in orbits:
-            orbits[orbit_pair[0]] = []
-        orbits[orbit_pair[0]].append(orbit_pair[1])
+        if orbit_pair[1] not in orbits:
+            orbits[orbit_pair[1]] = []
+        orbits[orbit_pair[1]].append(orbit_pair[0])
 
-        if orbit_pair[0] not in planets.keys():
-            planets[orbit_pair[0]] = 0
-        if orbit_pair[1] not in planets.keys():
-            planets[orbit_pair[1]] = 0
+if not "SAN" in orbits:
+    print("ERROR: santa not orbiting")
+if not "YOU" in orbits:
+    print("ERROR: you aren't orbiting")
 
-        if orbit_pair[0] == "COM":
-            lookups.append(orbit_pair[1])
-            planets[orbit_pair[1]] = 1
+for planet in orbits:
+    calculate_hops("YOU", 0, hops_from_you)
+    calculate_hops("SAN", 0, hops_from_san)
 
-for lookup in lookups:
-    add_orbits(lookup)
+hops = 0
+for planet in hops_from_san:
+    if planet in hops_from_you and (hops == 0 or hops_from_san[planet] + hops_from_you[planet] < hops):
+        hops = hops_from_san[planet] + hops_from_you[planet]
+        print("Found new ancestor: {}. Hops: {}. From San: {}. From you: {}".format(planet, hops, hops_from_san[planet], hops_from_you[planet]))
 
-print(lookups)
-print(orbits)
-print(planets)
-
-total = sum(planets[name] for name in planets)
-
-print(total)
+hops = hops-2
+print(hops)
 
