35c1579d809f99be43a80b3c4dc776ba44f3e8de
Deborah Martin <debomart@cisco.com>
2020-10-25 01:43:47 -0700

    solves part a in ~1m

--- 18/18.py
+++ 18/18.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import copy
+from collections import deque
 
 class MapWalker(object):
+    DIRS=[(1, 0), (0, -1), (0, 1), (-1, 0)]
+
     def __init__(self):
         with open("18/map.txt", "r") as f:
             lines = f.readlines()
@@ -10,56 +13,71 @@ class MapWalker(object):
         self.XMAX = len(self.map[0])
         self.YMAX = len(self.map)
         self.shortest = 0
+        self.keycombs = {}
 
     def _add_dir(self, coord, dir):
         new_coord = tuple(c + d for c, d in zip(coord, dir))
         return new_coord
 
-    def _find_possible_dirs(self, coord, map, camefrom=None):
-        poss_dirs = []
-
-        for dir in [(1, 0), (0, -1), (0, 1), (-1, 0)]:
+    def _find_possible_dirs(self, coord, visited, keysfound):
+        for dir in self.DIRS:
             x, y = self._add_dir(coord, dir)
 
-            if camefrom and (x, y) == camefrom:
+            if (x, y) in visited:
                 continue
 
             if x > self.XMAX or y > self.YMAX:
                 continue
 
-            asciicode = ord(map[y][x])
-            if asciicode != 35 and not (asciicode >= 65 and asciicode <= 90):
+            asciicode = ord(self.map[y][x])
+            if asciicode != 35 and not (asciicode >= 65 and asciicode <= 90 \
+               and chr(asciicode + 32) not in keysfound):
                 # not a wall or upper case letter
-                poss_dirs.append(dir)
+                yield dir
 
-        return poss_dirs
+    def _find_reachable_keys(self, coord, keysfound=""):
+        keys = {}
+        visited = set()
+        # do a BFS search for all reachable keys but keep track of num steps
+        d = deque([(coord, 0)])
+        while len(d) > 0:
+            node, steps = d.popleft()
+            visited.add(node)
+            # handling for if the node is a key
+            asciicode = ord(self.map[node[1]][node[0]])
+            if asciicode >= 97 and asciicode <= 122 and chr(asciicode) not in keysfound:
+                if node not in keys:
+                    keys[node] = steps
+                continue
 
-    def _shortest_from(self, coord, dir, map, steps):
-        steps += 1
-        if self.shortest != 0 and steps >= self.shortest:
-            return
+            # put all children nodes (not walls or doors) on the queue
+            for dir in self._find_possible_dirs(node, visited, keysfound):
+                d.append((self._add_dir(node, dir), steps + 1))
 
-        mapcpy = copy.deepcopy(map)
-        newcoord = self._add_dir(coord, dir)
-        asciicode = ord(mapcpy[newcoord[1]][newcoord[0]])
+        return keys
 
-        # found a key
-        if asciicode >= 97 and asciicode <= 122:
-            #print(f"found a key: {chr(asciicode)}")
-            mapcpy[newcoord[1]] = mapcpy[newcoord[1]].replace(chr(asciicode), ".")
-            for i, line in enumerate(mapcpy):
-                mapcpy[i] = line.replace(chr(asciicode - 32), ".")
-            # ok to go back on yourself if you've found a key
-            poss_dirs = self._find_possible_dirs(newcoord, mapcpy)
-        else:
-            poss_dirs = self._find_possible_dirs(newcoord, mapcpy, camefrom=coord)
+    def _shortest_from(self, coord, keysfound=""):
+        asciicode = ord(self.map[coord[1]][coord[0]])
+        assert(asciicode >= 97 and asciicode <= 122)
 
-        if not any(chr(a) in l for l in mapcpy for a in range(97, 123)):
-            print(f"all keys found! steps: {steps}")
-            self.shortest = steps
+        # Check whether this key combination has already been tried, if this
+        # route is not shorter, abort. This reduces path analysis and makes
+        # only function of state
+        keysfound = "".join(sorted(keysfound + chr(asciicode)))
 
-        for d in poss_dirs:
-            self._shortest_from(newcoord, d, mapcpy, steps)
+        if (keysfound, chr(asciicode)) in self.keycombs:
+            return self.keycombs[(keysfound, chr(asciicode))]
+
+        reachable_keys = self._find_reachable_keys(coord, keysfound)
+
+        if len(reachable_keys) == 0:
+            return 0
+
+        minsteps = min(keysteps + self._shortest_from(key, keysfound) \
+                       for key, keysteps in reachable_keys.items())
+        self.keycombs[(keysfound, chr(asciicode))] = minsteps
+
+        return minsteps
 
     def get_keys(self):
         for y, line in enumerate(self.map):
@@ -68,21 +86,18 @@ class MapWalker(object):
                 coord = (x, y)
                 break
 
-        poss_dirs = self._find_possible_dirs(coord, self.map)
-        assert(len(poss_dirs) > 0)
+        reachable_keys = self._find_reachable_keys(coord)
 
-        for d in poss_dirs:
-            self._shortest_from(coord, d, self.map, 0)
+        assert(len(reachable_keys) > 0)
 
-        print(f"shortest solution: {self.shortest}")
+        shortest = min(keysteps + self._shortest_from(key) for key, keysteps in reachable_keys.items())
+
+        print(f"shortest solution: {shortest}")
 
     def plot(self):
-
         for line in self.map:
             print(line)
 
 bot = MapWalker()
 bot.plot()
 bot.get_keys()
-
-
