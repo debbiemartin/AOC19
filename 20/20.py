df0d9439463e9543d5cbaa8c7eb393ede1ee800c
Deborah Martin <debomart@cisco.com>
2020-11-06 10:10:17 -0800

    20 part 2 done - <0.5seconds

--- 20/20.py
+++ 20/20.py
@@ -12,20 +12,36 @@ class MapWalker(object):
 
         self.map = {}
         self.portals = {}
+        self.reachable_portals_cache = {}
+        self.extent = {"xmin":0, "xmax":0, "ymin":0, "ymax":0}
         for y, line in enumerate(lines):
             for x, char in enumerate(line.strip("\n")):
                 self.map[(x, y)] = char
+                if char == ".":
+                    if self.extent["xmin"] == 0 or x < self.extent["xmin"]:
+                        self.extent["xmin"] = x
+                    if self.extent["xmax"] == 0 or x > self.extent["xmax"]:
+                        self.extent["xmax"] = x
+                    if self.extent["ymin"] == 0 or y < self.extent["ymin"]:
+                        self.extent["ymin"] = y
+                    if self.extent["ymax"] == 0 or y > self.extent["ymax"]:
+                        self.extent["ymax"] = y
                 
         for coord in self.map.keys():
             portal = self._is_portal(coord)
             if portal:
+                outer = (coord[0] == self.extent["xmin"] or  
+                         coord[0] == self.extent["xmax"] or 
+                         coord[1] == self.extent["ymin"] or 
+                         coord[1] == self.extent["ymax"])
+
                 if portal in self.portals:
-                    assert(self.portals[portal][1] == None)
-                    self.portals[portal] = (self.portals[portal][0], coord)
+                    assert(len(self.portals[portal]) == 1)
+                    self.portals[portal].append((coord, outer))
                 else:
                     # We just add the start and end like this - no portal 
                     # hopping as 2nd element always None
-                    self.portals[portal] = (coord, None)
+                    self.portals[portal] = [(coord, outer)]
 
     def _add_dir(self, coord, dir):
         new_coord = tuple(c + d for c, d in zip(coord, dir))
@@ -53,7 +69,7 @@ class MapWalker(object):
                 else:
                     portal = self.map[tempcoord2] + self.map[tempcoord]
                 
-                return portal
+                return portal 
                
         
     def _find_possible_dirs(self, coord, visited):
@@ -71,6 +87,10 @@ class MapWalker(object):
                 
                 
     def _find_reachable_portals(self, coord):
+        # cache now being called for different levels same coord     
+        if coord in self.reachable_portals_cache: 
+            return self.reachable_portals_cache[coord]
+        
         visited = set()
         d = deque([(coord, 0)])
         reachable_portals = {}
@@ -78,45 +98,77 @@ class MapWalker(object):
             node, steps = d.popleft()
             visited.add(node)
 
-            portal = self._is_portal(node)
-            if portal and node != coord:
-                reachable_portals[portal] = (steps, 0 if node == self.portals[portal][0] else 1)
+            # Search self.portals for the node
+            if node != coord:
+                for key, val in self.portals.items():
+                    if val[0][0] == node:
+                        reachable_portals[key] = (steps, 0)
+                    elif len(val) == 2 and val[1][0] == node:
+                        reachable_portals[key] = (steps, 1)
                     
             for dir in self._find_possible_dirs(node, visited):
                 d.append((self._add_dir(node, dir), steps + 1))
+        
+        self.reachable_portals_cache[coord] = reachable_portals
 
         return reachable_portals
+    
+    def _add_new_node(self, G, d, oldnode, newnode, weight):
+        if newnode[2] < 0 or \
+           newnode[0] == "AA" or \
+           (newnode[0] == "ZZ" and newnode[2] != 0):
+            return
+        if newnode not in G:
+            G.add_node(newnode)
+            if newnode[0] != "ZZ":
+                d.append(newnode)
+        if not G.has_edge(oldnode, newnode):
+            G.add_edge(oldnode, newnode, weight=weight)            
 
-    def shortest_path(self):
+    def shortest_path(self, recursive):
         G = nx.Graph()
-        d = deque([("AA", 0)])
-        G.add_node(("AA", 0))
+        # format: (portal name, portal index (0 or 1), maze level)
+        d = deque([("AA", 0, 0)]) 
+        G.add_node(("AA", 0, 0))
+        visited = []
 
         # Construct graph
         while len(d) > 0:
-            node, index = d.popleft()
-            
-            # add the other side of the portal to the graph and queue
-            if self.portals[node][1-index]:
-                if (node, 1-index) not in G:
-                    G.add_node((node, 1-index))
-                    d.append((node, 1-index))
-                if not G.has_edge((node, index), (node, 1-index)):
-                    G.add_edge((node, index), (node, 1-index), weight=1)
-            
+            node, index, level = d.popleft()
+                    
             for newnode, (dist, newindex)  in \
-                self._find_reachable_portals(self.portals[node][index]).items():
-
-                if (newnode, newindex) not in G: 
-                    G.add_node((newnode, newindex))
-                    d.append((newnode, newindex)) 
+                self._find_reachable_portals(self.portals[node][index][0]).items():
                 
-                if not G.has_edge((node, index), (newnode, newindex)):
-                    G.add_edge((node, index), (newnode, newindex), weight=dist) 
+                # add the other side of the portal to the graph and queue
+                if len(self.portals[newnode]) == 2:
+                    if recursive:
+                        newlevel = level + (-1 if self.portals[newnode][newindex][1] else 1)
+                        if newlevel > len(self.portals):
+                            continue
+                    else:
+                        newlevel = 0
 
-        print(nx.dijkstra_path_length(G, ("AA", 0), ("ZZ", 0), "weight"))
+                    self._add_new_node(G, d,
+                                       (node, index, level),
+                                       (newnode, 1-newindex, newlevel),
+                                       dist + 1)
+                elif newnode == "ZZ":
+                    self._add_new_node(G, d, 
+                                       (node, index, level),
+                                       (newnode, newindex, level),
+                                       dist)
+        
+        def astar_heur(node1, node2):
+            # this doesn't really make much difference.. 
+            return node1[2]
+
+        print(nx.astar_path_length(G, ("AA", 0, 0), ("ZZ", 0, 0), 
+                                   heuristic=astar_heur, weight="weight"))
 
 
-print("PART 1:")
 bot = MapWalker()
-bot.shortest_path()
+print("PART 1:")
+bot.shortest_path(False)
+
+print("PART 2:")
+bot.shortest_path(True)
